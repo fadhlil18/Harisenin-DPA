57d36c6adcdc7f70146b1f5685d00c986f8afe8a
fadhlil18 <fadhlilazfi@gmail.com>
2024-06-11 06:54:22 +0700

    Assignment CRUD nodejs

---
+++ seeders/20240610184905-User.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    const data = [];
+    for (let i = 0; i < 50; i++) {
+      data.push({
+        name: "Assignment" + i,
+        email: "email" + i + "@example.com",
+        password: "password" + i,
+        phone: i,
+        gender: Math.random() < 0.5 ? 'male' : 'female',
+      });
+    }
+
+    await queryInterface.bulkInsert("Users", data);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
